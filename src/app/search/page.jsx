d514d3c0243744d15f53021390145a97e00eeb29
Code-XT <rahuljhabokaro123@gmail.com>
2024-06-26 13:40:16 +0530

    direct fetching working

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -1,6 +1,24 @@
-const SearchPage = ({ searchParams }) => {
+import { db } from "@/db";
+import { productsTable } from "@/db/schema";
+import { sql } from "drizzle-orm";
+
+const SearchPage = async ({ searchParams }) => {
   const { query } = searchParams;
-  return <div>{query}</div>;
+  let products = await db
+    .select()
+    .from(productsTable)
+    .where(
+      sql`to_tsvector('simple', lower(${productsTable.name} || ' ' || ${
+        productsTable.description
+      })) @@ websearch_to_tsquery('simple', ${query
+        .trim()
+        .split(" ")
+        .join(" & ")
+        .toLowerCase()})`
+    )
+    .limit(5);
+
+  return <pre>{JSON.stringify(products, null, 2)}</pre>;
 };
 
 export default SearchPage;
